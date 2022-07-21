925f487a0bf7cb4440831ff8ade1cf807b62e32b
Melvin Rodriguez <melvinrr25@gmail.com>
2022-07-20 21:52:51 -0600

    env test

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,24 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ name }) {
+
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <nav>
-        <ul>
-          <li><a href="">1</a></li>
-          <li><a href="">2</a></li>
-          <li><a href="">3</a></li>
-          <li><a href="">4</a></li>
-          <li><a href="">5</a></li>
-        </ul>
-      </nav>
-
-      <main className={styles.main}>Melvin</main>
-
-      <footer className={styles.footer}>
-        hey there!
-      </footer>
+      <div>
+        
+        <strong>Username:</strong> { name }
+      </div>
     </div>
   )
+}
+
+export function getServerSideProps() {
+  return { 
+    props: { name: process.env.USERNAME } 
+  }
 }
